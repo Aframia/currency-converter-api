1c3a49dc29b5b78d138001ee8c1f1ca7ccbe7c56
Sandy's macbook pro <mac@Sandys-MacBook-pro.local>
2024-06-16 15:56:11 +0000

    attempt to setup model

--- init.py
+++ init.py
@@ -1,11 +1,14 @@
 import psycopg2
+from sqlalchemy.dialects.postgresql import JSON
+
+db = SQLAlchemy(app)
 
 conn = psycopg2.connect(
         host="localhost",
         database="postgres",
         user="postgres",
-        password="postgres",
-        port = "5434")
+        password="12345",
+        port = "5432")
 
 
 cur = conn.cursor()
@@ -18,6 +21,23 @@ cur.execute('CREATE TABLE rates (id serial PRIMARY KEY,'
                                  'rate NUMERIC(6,2) NOT NULL,'
                                  'date_added date DEFAULT CURRENT_TIMESTAMP);'
                                  )
+
+
+
+class Result(db.Model):
+ def __init__(self,id,to_currency,from_currency,rates):
+     id = db.Column(db.Integer, primary_key=True)
+     from_currency = db.Column(db.String())
+     to_currency = db.Column(JSON)
+     rates = db.Column(JSON)
+
+def __init__(self,to_currency,from_currency,rate):
+     self.to_currency = to_currency
+     self.from_currency = from_currency
+     self.rate = rate
+
+def __repr__(self):
+ return '<id {}>'.format(self.id)
 
 conn.commit()
 
